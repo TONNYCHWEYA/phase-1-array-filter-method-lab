b84faaa66ef0be42fa44ed8e35c1310b66f3b507
TONNYCHWEYA <chweyatonny@gmail.com>
2022-06-11 07:00:25 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution here
+function findMatching(array, name){
+return array.filter(function(string){
+
+  return ((string.toLowerCase()) === name.toLowerCase())
+})
+}
+function fuzzyMatch(array, letters){
+  return array.filter(function(letter){
+    if(letter.indexOf(letters)===0){
+      return true;
+    }else if(letter.indexOf(letters) > 0){
+      return false;
+    }
+  })
+}
+function matchName(array, string){
+  return array.filter(function(element){
+    return element.name === string
+  })
+
+}
